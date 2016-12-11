828666eb490eb49bff37515155cdc9d55c38fdd1
Diogo Korok <korokdiogo@gmail.com>
2016-12-11 19:42:25 -0200

    I haven't touched this

--- src/matrizesesistemas/Windows.java
+++ src/matrizesesistemas/Windows.java
@@ -1,4 +1,5 @@
 package matrizesesistemas;
+
 import java.io.File;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -12,76 +13,80 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-
-/** 
+/**
  * @author Leu <@leunardosevla at twitter.com>
- **/
+ *
+ */
 public class Windows extends Application {
+
     public static void main(String[] args) {
         launch(args);
         //Main adicionada só pra testes!!!!!
     }
-    
+
     @Override
-    public void start(Stage stage){
+    public void start(Stage stage) {
         Button buttonSum = new Button();
         Button buttonMultiply = new Button();
         Button buttonMinus = new Button();
         Button buttonTransposition = new Button();
         Button buttonShowMatrix = new Button();
-        
+
         buttonSum.setText("Somar");
         buttonMultiply.setText("Multiplicar");
         buttonMinus.setText("Subtrair");
         buttonTransposition.setText("Transposição");
         buttonShowMatrix.setText("Exibir Matriz");
-        
+
         buttonSum.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         buttonMinus.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         buttonMultiply.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         buttonTransposition.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         buttonShowMatrix.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        
-        
+
         buttonSum.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Work in progress");
             }
+
         });
-        
+
         buttonMultiply.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Work in progress");
             }
+
         });
-        
+
         buttonMinus.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Work in progress");
             }
+
         });
-        
+
         buttonTransposition.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Work in progress");
             }
+
         });
-        
-        buttonShowMatrix.setOnAction(new EventHandler<ActionEvent>(){
-            
+
+        buttonShowMatrix.setOnAction(new EventHandler<ActionEvent>() {
+
             @Override
-            public void handle(ActionEvent event){
+            public void handle(ActionEvent event) {
                 Stage stageShowMatrix = new Stage();
-                stageShowMatrix.setTitle("Exibir matriz");                
-            
+                stageShowMatrix.setTitle("Exibir matriz");
+
                 FileChooser fileChooser = new FileChooser();
                 fileChooser.setTitle("Selecione a matriz a ser exibida:");
 
@@ -91,28 +96,30 @@ public class Windows extends Application {
                 File matrix = fileChooser.showOpenDialog(stageShowMatrix);
                 //TODO: retornar esse file matrix pra ser exibido em outra janela efetivamente.
             }
+
         });
-        
+
         Text t = new Text();
         t.setText("Escolha a operação:");
-        
+
         GridPane grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setAlignment(Pos.CENTER);
-        grid.add(t, 0,0);
+        grid.add(t, 0, 0);
         grid.add(buttonSum, 1, 2);
         grid.add(buttonMinus, 2, 2);
         grid.add(buttonMultiply, 3, 2);
         grid.add(buttonTransposition, 4, 2);
         grid.add(buttonShowMatrix, 5, 2);
-        
-        Scene scene = new Scene(grid, 600, 100);        
-        
+
+        Scene scene = new Scene(grid, 600, 100);
+
         stage.setTitle("Calculadora de Matrizes");
         stage.setScene(scene);
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/matrix-icon.jpg")));
         stage.show();
-    }    
-    
+
+    }
+
 }
