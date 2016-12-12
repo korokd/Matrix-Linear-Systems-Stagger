9d05eb4028d605f196191874f744ee817880738e
Diogo Korok <korokdiogo@gmail.com>
2016-12-12 20:55:55 -0200

    style: alt+shift+f

--- src/controllers/OperationsController.java
+++ src/controllers/OperationsController.java
@@ -1,6 +1,4 @@
-
 package controllers;
-
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,66 +15,66 @@ import matrizesesistemas.InvalidMatrixesException;
 import matrizesesistemas.Actions;
 import matrizesesistemas.Main;
 import matrizesesistemas.NotMatchingSizesException;
-        
-/** 
- * @author Leu <@leunardosevla at twitter.com>
- **/
 
-public class OperationsController {  
-    
-    public void sum (Main main) throws IOException, NotMatchingSizesException{
-        for(int i = 0; i < main.getMatrixes().size(); i++){
-            Actions.matrixSum( main.getMatrixes().get(i), main.getMatrixes().get(i+1));
-        }        
+/**
+ * @author Leu <@leunardosevla at twitter.com>
+ *
+ */
+public class OperationsController {
+
+    public void sum(Main main) throws IOException, NotMatchingSizesException {
+        for (int i = 0; i < main.getMatrixes().size(); i++) {
+            Actions.matrixSum(main.getMatrixes().get(i), main.getMatrixes().get(i + 1));
+        }
         generateMatrixStage();
         //TODO: add methods to sum matrixes
     }
-    
-    public void minus (Main main) throws IOException, NotMatchingSizesException {
-        for(int i = 0; i < main.getMatrixes().size(); i++){
-            Actions.matrixMinus( main.getMatrixes().get(i), main.getMatrixes().get(i+1));
-        }        
+
+    public void minus(Main main) throws IOException, NotMatchingSizesException {
+        for (int i = 0; i < main.getMatrixes().size(); i++) {
+            Actions.matrixMinus(main.getMatrixes().get(i), main.getMatrixes().get(i + 1));
+        }
         generateMatrixStage();
-         //TODO: add methods to minus matrixes
+        //TODO: add methods to minus matrixes
     }
-    
-    public void multiply (Main main) throws IOException, NotMatchingSizesException {
-        for(int i = 0; i < main.getMatrixes().size(); i++){
-            Actions.matrixProduct( main.getMatrixes().get(i), main.getMatrixes().get(i+1));
-        }        
+
+    public void multiply(Main main) throws IOException, NotMatchingSizesException {
+        for (int i = 0; i < main.getMatrixes().size(); i++) {
+            Actions.matrixProduct(main.getMatrixes().get(i), main.getMatrixes().get(i + 1));
+        }
         generateMatrixStage();
-         //TODO: add methods to multiply matrixes
+        //TODO: add methods to multiply matrixes
     }
-    
-    public void transposition (Main main) throws IOException, InvalidMatrixesException {
-        
-        if(main.getMatrixes().size() > 1){
+
+    public void transposition(Main main) throws IOException, InvalidMatrixesException {
+
+        if (main.getMatrixes().size() > 1) {
             throw new InvalidMatrixesException("Selecione apenas uma matriz.");
         } else {
             Actions.matrixTrans(main.getMatrixes().get(0));
             generateMatrixStage();
 
         }
-         //TODO: add methods to transpose matrixes
+        //TODO: add methods to transpose matrixes
     }
-    
-    public void showMatrix (Main main) throws IOException, InvalidMatrixesException {
-        if(main.getMatrixes().size() > 1){
+
+    public void showMatrix(Main main) throws IOException, InvalidMatrixesException {
+        if (main.getMatrixes().size() > 1) {
             throw new InvalidMatrixesException("Selecione apenas uma matriz.");
         } else {
             Actions.printMatrix(main.getMatrixes().get(0));
             generateMatrixStage();
 
         }
-         //TODO: add methods to show matrix
+        //TODO: add methods to show matrix
     }
-    
+
     private void generateMatrixStage() throws IOException {
         Stage operationsStage = new Stage();
         URL arquivoFXML = getClass().getResource("../fxml/generated-matrix.fxml");
         Parent fxmlParent = (Parent) FXMLLoader.load(arquivoFXML);
         operationsStage.getIcons().add(new Image(getClass()
-            .getResourceAsStream("/images/matrix-icon.jpg")));
+                .getResourceAsStream("/images/matrix-icon.jpg")));
         operationsStage.setScene(new Scene(fxmlParent, 600, 600));
         operationsStage.show();
     }
--- src/controllers/SelectFileController.java
+++ src/controllers/SelectFileController.java
@@ -1,4 +1,3 @@
-
 package controllers;
 
 import java.io.File;
@@ -18,51 +17,54 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import matrizesesistemas.Main;
 
-/** 
+/**
  * @author Leu <@leunardosevla at twitter.com>
- **/
+ *
+ */
 public class SelectFileController {
+
     private ObservableList<String> nomeArquivos = observableArrayList(); //lista para printar o nome no ListView
     static List<File> selectedFiles = new ArrayList<File>(); //lista dos arquivos
-    
+
     @FXML
     ListView<String> listViewId;
-    
-    public void add(){
-        
+
+    public void add() {
+
         Stage stage = new Stage();
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Selecione a matriz a ser exibida:");
-        
+
         FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("Arquivo de texto txt (.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extensionFilter);
-         
+
         List<File> files = fileChooser.showOpenMultipleDialog(stage);
-        
-        if(files != null){
+
+        if (files != null) {
             //o for adiciona os arquivos na lista selectedFiles E também
             //o seu nome no nomeArquivos.
-            for(File file : files) {                
+            for (File file : files) {
                 nomeArquivos.clear();
                 nomeArquivos.add(file.getName());
                 selectedFiles.add(file);
                 listViewId.getItems().addAll(nomeArquivos);
                 Main.addToMatrixes(file);
-            }     
-        }    
-        
-}
-    public void confirm() throws IOException{
-        
-        if(!selectedFiles.isEmpty()){
+            }
+        }
+
+    }
+
+    public void confirm() throws IOException {
+
+        if (!selectedFiles.isEmpty()) {
             Stage operationsStage = new Stage();
             URL arquivoFXML = getClass().getResource("../fxml/operations.fxml");
             Parent fxmlParent = (Parent) FXMLLoader.load(arquivoFXML);
             operationsStage.getIcons().add(new Image(getClass()
-                .getResourceAsStream("/images/matrix-icon.jpg")));
+                    .getResourceAsStream("/images/matrix-icon.jpg")));
             operationsStage.setTitle("Calculadora de matrizes");
             operationsStage.setScene(new Scene(fxmlParent, 237, 204));
             operationsStage.show();
         }
-    }    
+    }
 }
